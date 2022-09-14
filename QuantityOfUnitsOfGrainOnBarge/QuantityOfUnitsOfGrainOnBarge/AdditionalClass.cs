c6d01b2544368266db2a4874bbebc63ad2c3cc36
DestroxUA <106181199+DestroxUA@users.noreply.github.com>
2022-09-14 07:09:04 +0300

    QuantityOfUnitsOfGrainOnBarge
    QuantityOfUnitsOfGrainOnBarge

---
+++ QuantityOfUnitsOfGrainOnBarge/QuantityOfUnitsOfGrainOnBarge/AdditionalClass.cs
@@ -0,0 +1,69 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace QuantityOfUnitsOfGrainOnBarge
+{
+    internal class AdditionalClass
+    {
+        public static int ReadIntOnly()
+        {
+            ConsoleKeyInfo key;
+            var rule = @"[0-9]";
+            var sb = new StringBuilder();
+
+            while (true)
+            {
+                key = Console.ReadKey(true);
+                if (key.Key == ConsoleKey.Enter)
+                {
+                    Console.WriteLine();
+                    break;
+                }
+
+                if (Regex.IsMatch(key.KeyChar.ToString(), rule))
+                {
+                    sb.Append(key.KeyChar);
+                    Console.Write(key.KeyChar);
+                }
+
+                if (key.Key == ConsoleKey.Backspace)
+                {
+                    int PreviosLength = sb.Length;
+                    sb.Remove(sb.Length - 1, 1);
+                    Console.SetCursorPosition(0, Console.CursorTop);
+                    Console.WriteLine(new String(' ', PreviosLength));
+                    Console.SetCursorPosition(0, Console.CursorTop - 1);
+                    Console.WriteLine(sb);
+                    Console.SetCursorPosition(sb.Length, Console.CursorTop - 1);
+                }
+            }
+
+            if (sb.Length==0)
+
+                return 0;
+        
+            else
+
+                return Convert.ToInt32(sb.ToString());
+        }
+
+        public static int CalculateTheQuantityOfUnitsOfGrain(ref int LowerLevelValue, int CurrentLevelValue)
+        {
+            int buf = 0;
+            int result;
+
+            if (LowerLevelValue > CurrentLevelValue)
+            {
+
+                result = LowerLevelValue - CurrentLevelValue;
+                buf += result;
+            }
+            else
+            {
+                LowerLevelValue = CurrentLevelValue;
+            }
+
+            return buf;
+        }
+    }
+}
---
+++ QuantityOfUnitsOfGrainOnBarge/QuantityOfUnitsOfGrainOnBarge/Program.cs
@@ -0,0 +1,55 @@
+﻿using QuantityOfUnitsOfGrainOnBarge;
+
+Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+int QuantityOfUnitsOfGrain = 0;
+int i = 0;
+int HigherLevelValue = 0;
+int HigherLevelIndex = 0;
+int LowerLevelValue = 0;
+
+Console.WriteLine("Уведіть кількість рівнів баржі");
+int QuantityLevels = AdditionalClass.ReadIntOnly();
+int[] LevelsOfHeight = new int[QuantityLevels];
+
+if (QuantityLevels == 0)
+{
+    Console.WriteLine($"Кількість одиниць зерна дорівнюе {QuantityOfUnitsOfGrain}");
+    return;
+}
+
+Console.WriteLine("Уведіть розміры высот рівнів баржі");
+
+for (i = 0; i < QuantityLevels; i++)
+{
+    LevelsOfHeight[i] = AdditionalClass.ReadIntOnly();
+    if (HigherLevelValue < LevelsOfHeight[i])
+    {
+        HigherLevelValue = LevelsOfHeight[i];
+        HigherLevelIndex = i;
+    }
+}
+
+
+if (LevelsOfHeight.Length <= 2)
+{
+    Console.WriteLine($"Кількість одиниць зерна дорівнюе {QuantityOfUnitsOfGrain}");
+}
+else
+{
+    LowerLevelValue = LevelsOfHeight[0];
+
+    for (i = 0; i < HigherLevelIndex; i++)
+    {
+        QuantityOfUnitsOfGrain += AdditionalClass.CalculateTheQuantityOfUnitsOfGrain(ref LowerLevelValue, LevelsOfHeight[i]);
+    }
+
+    LowerLevelValue = LevelsOfHeight[QuantityLevels - 1];
+    for (i = QuantityLevels - 1; i > HigherLevelIndex; i--)
+    {
+        QuantityOfUnitsOfGrain += AdditionalClass.CalculateTheQuantityOfUnitsOfGrain(ref LowerLevelValue, LevelsOfHeight[i]);
+    }
+
+    Console.WriteLine($"Кількість одиниць зерна дорівнюе {QuantityOfUnitsOfGrain}");
+}
+
